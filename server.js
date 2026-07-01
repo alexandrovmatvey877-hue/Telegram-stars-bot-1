ff5d22ba7a572efd3cc137a569dccc9148ebc07a
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-01 13:46:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
 app.use(express.json());
 app.use((req, res, next) => {
