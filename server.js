4d9ce554e62e5c359d0b658f958ae1e014333d0d
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-01 13:47:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-
 app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.method, req.url);
