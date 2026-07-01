9b1831a7470734237f33ccc763f705f67dc614a4
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-01 14:20:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -155,8 +155,8 @@ app.get("/users", (req, res) => {
 
 app.post("/set-balance", (req, res) => {
 
-    let { telegram_id, username } = req.body;
-telegram_id = String(telegram_id).replace(".0", "");
+    let { telegram_id, balance } = req.body;
+    telegram_id = String(telegram_id).replace(".0", "");
 
     if (!telegram_id) {
         return res.status(400).json({
