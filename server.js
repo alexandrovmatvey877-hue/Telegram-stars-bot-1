dbf8252a5972df5feee73d96eec93391a02983dc
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 01:55:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const ADMIN_KEY = "white-stars-super-admin";
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
