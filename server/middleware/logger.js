0585605db47dfe7b1277c3b07da064b636b114f9
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 20:12:38 +0300

    Update logger.js

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,1 +1,25 @@
-.
+// =====================================
+// WHITE STARS Logger
+// =====================================
+
+function logger(req, res, next) {
+
+    const start = Date.now();
+
+    res.on("finish", () => {
+
+        const time = Date.now() - start;
+
+        const date = new Date().toLocaleString("ru-RU");
+
+        console.log(
+            `[${date}] ${req.method} ${req.originalUrl} | ${res.statusCode} | ${time} ms`
+        );
+
+    });
+
+    next();
+
+}
+
+module.exports = logger;
