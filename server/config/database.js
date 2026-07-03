87425b07822e9e09d08f901f51079653218000d3
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 20:11:41 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,39 @@
-.
+const { Pool } = require("pg");
+
+// ==============================
+// PostgreSQL
+// ==============================
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// Проверка подключения
+async function connectDatabase() {
+
+    try {
+
+        const client = await pool.connect();
+
+        console.log("✅ PostgreSQL подключена");
+
+        client.release();
+
+    } catch (error) {
+
+        console.error("❌ Ошибка подключения к PostgreSQL");
+        console.error(error);
+
+        process.exit(1);
+
+    }
+
+}
+
+module.exports = {
+    pool,
+    connectDatabase
+};
