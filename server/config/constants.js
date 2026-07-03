7513386383bf1e2a49886023da25a8e104e6bd3b
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 20:10:06 +0300

    Update constants.js

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,1 +1,21 @@
-.
+// ==============================
+// WHITE STARS
+// Константы проекта
+// ==============================
+
+const PORT = process.env.PORT || 3000;
+
+const ADMIN_KEY =
+    process.env.ADMIN_KEY ||
+    "white-stars-super-admin";
+
+const APP_NAME = "WHITE STARS";
+
+const VERSION = "5.0.0";
+
+module.exports = {
+    PORT,
+    ADMIN_KEY,
+    APP_NAME,
+    VERSION
+};
