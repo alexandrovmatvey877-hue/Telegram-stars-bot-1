3be0e5760a2f3896af333cca60f244d604c90332
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 20:21:46 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,18 @@
-.
+const express = require("express");
+const router = express.Router();
+
+const usersController = require("../controllers/usersController");
+
+// Регистрация / авторизация пользователя
+router.post("/register", usersController.register);
+
+// Получить профиль
+router.get("/:telegram_id", usersController.getProfile);
+
+// Обновить пользователя
+router.put("/:telegram_id", usersController.updateProfile);
+
+// Статистика пользователя
+router.get("/:telegram_id/stats", usersController.getStats);
+
+module.exports = router;
