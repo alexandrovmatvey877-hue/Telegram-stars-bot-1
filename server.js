11e5ca116d9b2c932f14e225a30379cee7811739
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-03 14:33:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -401,6 +401,14 @@ app.post("/wallet", async (req, res) => {
              SET wallet_address = $1
              WHERE telegram_id = $2`,
             [wallet_address, telegram_id]
+const check = await pool.query(
+    `SELECT telegram_id, wallet_address
+     FROM users
+     WHERE telegram_id = $1`,
+    [telegram_id]
+);
+
+console.log("После UPDATE:", check.rows);
         );
 
         res.json({
