003cfe05b16a8ab3e7d8c59d21449a5a2d5a951d
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-04 14:32:28 +0300

    Update admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,1 +1,15 @@
-.
+const express = require("express");
+const router = express.Router();
+
+const adminController = require("../controllers/adminController");
+
+// Получить всех пользователей
+router.get("/users", adminController.getUsers);
+
+// Изменить баланс пользователя
+router.post("/balance", adminController.updateBalance);
+
+// Статистика
+router.get("/stats", adminController.getStats);
+
+module.exports = router;
