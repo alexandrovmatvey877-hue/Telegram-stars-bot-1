ea978e9e938b7487d0daf8b79c5db229dc81d683
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-04 11:58:29 +0300

    Update wallet.js

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -1,1 +1,16 @@
-.
+const express = require("express");
+
+const router = express.Router();
+
+const walletController = require("../controllers/walletController");
+
+// Получить кошелек пользователя
+router.get("/:telegram_id", walletController.getWallet);
+
+// Сохранить кошелек
+router.post("/", walletController.saveWallet);
+
+// Удалить кошелек
+router.delete("/:telegram_id", walletController.deleteWallet);
+
+module.exports = router;
