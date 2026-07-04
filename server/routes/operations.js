4c26c5cd45e961779035b032e5799b7217acde23
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-04 12:02:10 +0300

    Update operations.js

--- server/routes/operations.js
+++ server/routes/operations.js
@@ -1,1 +1,34 @@
-.
+const express = require("express");
+
+const router = express.Router();
+
+const operationsController = require("../controllers/operationsController");
+const adminAuth = require("../middleware/adminAuth");
+
+// Все операции (админ)
+router.get(
+    "/",
+    adminAuth,
+    operationsController.getAllOperations
+);
+
+// Операции пользователя
+router.get(
+    "/user/:telegram_id",
+    operationsController.getUserOperations
+);
+
+// Добавить операцию
+router.post(
+    "/",
+    operationsController.createOperation
+);
+
+// Получить одну операцию
+router.get(
+    "/:id",
+    adminAuth,
+    operationsController.getOperation
+);
+
+module.exports = router;
