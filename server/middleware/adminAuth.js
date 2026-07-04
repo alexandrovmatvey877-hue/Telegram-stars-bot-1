991aa8a24b4e1b47ae28ff994ee89fa6c1ddd394
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-04 12:06:49 +0300

    Update adminAuth.js

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -1,1 +1,20 @@
-.
+const ADMIN_KEY = process.env.ADMIN_KEY || "white-stars-super-admin";
+
+module.exports = (req, res, next) => {
+
+    const key = req.headers["x-admin-key"];
+
+    if (!key || key !== ADMIN_KEY) {
+
+        return res.status(403).json({
+
+            success: false,
+            message: "Access denied"
+
+        });
+
+    }
+
+    next();
+
+};
