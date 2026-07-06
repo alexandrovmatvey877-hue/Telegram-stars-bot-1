ca75fbbe7e1d61345b9f748d33e9ac682263fb62
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-06 15:07:11 +0300

    Update helpers.js

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,1 +1,9 @@
-.
+// a natural ya Nujen?
+// да хз, потом мб пригодишься 
+// a Potomac eto kinda?
+// а хуй знает
+//*cry" 
+// ты чо срешь далбабеб
+//suka cry eto plakat a ne srat 
+// аа, тоесть плоке плоке нормалдаке?
+// poshel nahuy
