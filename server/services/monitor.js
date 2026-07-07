fecc6943bccf1a97e9a71be8f14e3c5be6f8f5d9
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-07 23:46:46 +0300

    Update monitor.js

--- server/services/monitor.js
+++ server/services/monitor.js
@@ -1,1 +1,29 @@
 // наверное до этого я никогда не найду
+const fetch = require("node-fetch");
+
+async function getTonPrice() {
+
+    try {
+
+        const response = await fetch(
+            "https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=rub"
+        );
+
+        const data = await response.json();
+
+        return data["the-open-network"].rub;
+
+    } catch (err) {
+
+        console.error("TON PRICE ERROR:", err);
+
+        return null;
+
+    }
+
+}
+
+module.exports = {
+    getTonPrice
+};
+//нихуя себе... дошел...
