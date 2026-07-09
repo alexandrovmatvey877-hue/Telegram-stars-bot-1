b770234e74e989bd0c9849b667aae250716f859c
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-09 11:32:23 +0300

    Update fragment.js

--- server/services/fragment.js
+++ server/services/fragment.js
@@ -1,1 +1,17 @@
-// тут наверное будетм подключен фрагмент
+async function getStarPriceTon() {
+
+    const response = await fetch(
+        "https://fragment-api.ydns.eu:8443/api/v1/prices"
+    );
+
+    const data = await response.json();
+
+    return Number(
+        data.stars.price_with_commission_kyc
+    );
+
+}
+
+module.exports = {
+    getStarPriceTon
+};
