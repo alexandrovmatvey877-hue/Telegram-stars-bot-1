8b4cec8c0e208e0ce1ae4b35b787a6d84b3f80db
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-12 15:54:19 +0300

    Update premium.js

--- js/premium.js
+++ js/premium.js
@@ -214,6 +214,15 @@ function buyPremium(){
 
 }
 
+function goBack() {
+
+    if (window.history.length > 1) {
+        window.history.back();
+    } else {
+        window.location.href = "../index.html";
+    }
+
+}
 
 
 loadPremium();
