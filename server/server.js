87525552b029e7c5915f7cbf8104b2e415d3bed2
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-13 12:22:50 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,13 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 // =========================
+// SECURITY
+// =========================
+
+// Не показывать, что сервер работает на Express
+app.disable("x-powered-by");
+
+// =========================
 // DATABASE
 // =========================
 
@@ -25,7 +32,7 @@ const db = require("./config/database");
 app.use(cors());
 
 app.use(express.json({
-    limit: "10mb"
+    limit: "1mb"
 }));
 
 app.use(express.urlencoded({
