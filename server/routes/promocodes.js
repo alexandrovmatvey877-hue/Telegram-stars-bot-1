e4adcbb5add5cc0dcc193dd5a82694e2d2e098db
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-15 18:03:23 +0300

    Update promocodes.js

--- server/routes/promocodes.js
+++ server/routes/promocodes.js
@@ -12,43 +12,4 @@ router.get("/", promocodeController.getPromos);
 
 router.get("/:code", promocodeController.getPromo);
 
-router.get("/test/create", async (req, res) => {
-    const db = require("../config/database");
-
-    try {
-        const result = await db.query(
-            `
-            INSERT INTO promocodes
-            (
-                code,
-                type,
-                value,
-                max_uses
-            )
-            VALUES
-            ($1,$2,$3,$4)
-            RETURNING *
-            `,
-            [
-                "DRUN",
-                "balance",
-                50,
-                1000
-            ]
-        );
-
-        res.json({
-            success:true,
-            promo:result.rows[0]
-        });
-
-    } catch(err) {
-        console.error(err);
-        res.status(500).json({
-            success:false,
-            error:err.message
-        });
-    }
-});
-
 module.exports = router;
