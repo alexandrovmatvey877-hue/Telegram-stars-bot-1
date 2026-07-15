4830cdde305f5cb905efe013cbb6ab89cc25f581
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-16 00:17:57 +0300

    Update initDatabase.js

--- server/database/initDatabase.js
+++ server/database/initDatabase.js
@@ -332,6 +332,11 @@ WHERE NOT EXISTS(
 SELECT 1 FROM wheel_prizes
 );
 
+await db.query(`
+ALTER TABLE wheel_prizes
+ADD COLUMN IF NOT EXISTS color TEXT DEFAULT '#ffffff';
+`);
+
 `);
 
 await db.query(`
