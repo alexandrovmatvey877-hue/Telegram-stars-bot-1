7943fce6bb0ddba28202624f19bf2bf2284f8139
alexandrovmatvey877-hue <alexandrovmatvey877@gmail.com>
2026-07-18 23:26:52 +0300

    Update wheelAdmin.js

--- js/wheelAdmin.js
+++ js/wheelAdmin.js
@@ -27,13 +27,27 @@ alert(JSON.stringify(data));
             (data.prizes || []).forEach(p => {
 
                 box.innerHTML += `
-                    <div class="wheel-prize-card">
-                        <b>${p.name}</b><br>
-                        Тип: ${p.type}<br>
-                        Значение: ${p.value}<br>
-                        Шанс: ${p.chance}%
-                    </div>
-                `;
+<div class="wheel-prize-card">
+
+<div style="
+width:20px;
+height:20px;
+background:${p.color};
+border-radius:4px;
+display:inline-block;
+margin-right:10px;
+"></div>
+
+<b>${p.name}</b><br>
+
+Тип: ${p.type}<br>
+
+Значение: ${p.value}<br>
+
+Шанс: ${p.chance}%
+
+</div>
+`;
 
             });
 
